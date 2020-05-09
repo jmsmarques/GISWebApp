3ecdfcf4bb69b2b04c8a9a1faecbd98de6cb5c93
Jorge Miguel Da Silva Marques <jms@student.dei.uc.pt>
2020-05-09 16:17:20 +0100

    updated allowed hosts

--- gisWebApp/settings.py
+++ gisWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'du77velyn=g0lrw@twxysk^p71(6jb+ysh-g!p2%807rlq6+q!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['giswebapp.herokuapp.com']
 
 
 # Application definition
